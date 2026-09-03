ceef0f4de86978dcac066570aac04adf3f7d4335
SHASHI <shashivashist902@gmail.com>
2026-09-04 04:34:28 +0530

    refactor(terminal): remove unused handler type import

--- src/components/terminal/handlers/shellCommands.ts
+++ src/components/terminal/handlers/shellCommands.ts
@@ -1,5 +1,5 @@
 
-import type { CommandExecutionContext, TerminalOutputItem } from '../terminalTypes';
+import type { TerminalOutputItem } from '../terminalTypes';
 import type { TerminalCommandHandler } from '../commandTypes';
 
 export const SHELL_COMMANDS = new Set([
